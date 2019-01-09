efa6b49d699af1af8ebae62b2618fd6d01b8f5d3
chintu9595 <45651779+chintu9595@users.noreply.github.com>
2019-01-09 12:56:05 +0530

    Update plan.sh

--- webapp-proxy/plan.sh
+++ webapp-proxy/plan.sh
@@ -3,7 +3,7 @@ pkg_name=webapp-proxy
 pkg_deps=(core/nginx)
 pkg_version="0.1.0"
 pkg_svc_run="nginx -c ${pkg_svc_config_path}/nginx.conf"
-pkg_svc_user="root"
+#pkg_svc_user="root"
 
 pkg_binds=(
   [app]="port"
